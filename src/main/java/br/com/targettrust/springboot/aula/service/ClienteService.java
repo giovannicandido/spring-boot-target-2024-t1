d9fb1222e2b87a7ac7a6fc41a8d16ed227ddccde
Giovanni Silva <git@gsilva.pro>
2024-05-23 21:42:41 -0300

    aula 13: implement interface for services

--- src/main/java/br/com/targettrust/springboot/aula/service/ClienteService.java
+++ src/main/java/br/com/targettrust/springboot/aula/service/ClienteService.java
@@ -1,71 +1,20 @@
 package br.com.targettrust.springboot.aula.service;
 
 import br.com.targettrust.springboot.aula.model.Cliente;
-import br.com.targettrust.springboot.aula.model.Endereco;
-import br.com.targettrust.springboot.aula.model.exceptions.ExercicioIdsNotFoundException;
-import br.com.targettrust.springboot.aula.repository.ClienteRepository;
-import br.com.targettrust.springboot.aula.repository.ExercicioRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
-@RequiredArgsConstructor
-@Slf4j
-public class ClienteService {
-    private final ClienteRepository clienteRepository;
-    private final ExercicioRepository exercicioRepository;
-    private final ExercicioService exercicioService;
-    private final CepService cepService;
+public interface ClienteService {
+    List<Cliente> listClientes(String nome);
 
-    @Transactional(readOnly = true)
-    public List<Cliente> listClientes(String nome) {
-//        exercicioRepository.deleteA
-        var clientes =  clienteRepository.findAll();
-        for (Cliente cliente : clientes) {
-            List<Endereco> enderecos = cliente.getEnderecos();
-            enderecos.forEach(endereco -> log.info(endereco.toString()));
-        }
-        return clientes;
+    Cliente createCliente(Cliente cliente, String cep);
 
-    }
+    Optional<Cliente> findById(Long id);
 
-    // 1. Organization
-    //   2. Policies
-    //   3. Policy Options
-    //      4. Calculation Methods
-    //        5. Inputs
-    //        5. Results
+    Optional<Cliente> editCliente(Long id, Cliente cliente);
 
-    @Transactional(noRollbackFor = ExercicioIdsNotFoundException.class)
-    public Cliente createCliente(Cliente cliente, String cep) {
-        Endereco endereco = cepService.searchAddress(cep);
-        if(endereco != null) {
-            cliente.getEnderecos().add(endereco);
-        }
-        var clienteSaved = clienteRepository.create(cliente);
-//        exercicioService.associarExercicios(200L, List.of(3L));
-        return clienteSaved;
-    }
+    Optional<Cliente> editarParcial(Long id, Cliente cliente);
 
-    public Optional<Cliente> findById(Long id) {
-        return clienteRepository.findById(id);
-    }
-
-    public Optional<Cliente> editCliente(Long id, Cliente cliente) {
-        return Optional.ofNullable(clienteRepository.update(cliente, id));
-    }
-
-    public Optional<Cliente> editarParcial(Long id, Cliente cliente) {
-        // Edicao teria que ser parcial
-        return Optional.ofNullable(clienteRepository.update(cliente, id));
-    }
-
-    public void deletarCliente(Long id) {
-       clienteRepository.deleteById(id);
-    }
+    void deletarCliente(Long id);
 }
--- src/main/java/br/com/targettrust/springboot/aula/service/ExercicioService.java
+++ src/main/java/br/com/targettrust/springboot/aula/service/ExercicioService.java
@@ -1,88 +1,13 @@
 package br.com.targettrust.springboot.aula.service;
 
 import br.com.targettrust.springboot.aula.model.Exercicio;
-import br.com.targettrust.springboot.aula.model.exceptions.ExercicioIdsNotFoundException;
-import br.com.targettrust.springboot.aula.model.exceptions.RegistryNotFoundException;
-import br.com.targettrust.springboot.aula.repository.ClienteRepository;
-import br.com.targettrust.springboot.aula.repository.ExercicioRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.TransactionTemplate;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-public class ExercicioService {
+public interface ExercicioService {
+    Exercicio createExercicio(Exercicio exercicio);
 
-    private final ExercicioRepository exercicioRepository;
-    private final ClienteRepository clienteRepository;
-    private final PlatformTransactionManager transactionManager;
-    private final TransactionTemplate transactionTemplate;
+    void associarExercicios(Long idCliente, List<Long> idExercicios);
 
-//    @PostConstruct
-//    private void init() {
-//        transactionTemplate = new TransactionTemplate(transactionManager);
-//    }
-
-    @Transactional
-    public Exercicio createExercicio(Exercicio exercicio) {
-        return exercicioRepository.save(exercicio);
-//        exercicio.setId(99L);
-//        return exercicio;
-    }
-
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public void associarExercicios(Long idCliente, List<Long> idExercicios) {
-        // ir no banco localizar a Cliente
-        var cliente = clienteRepository.findById(idCliente)
-                .orElseThrow(() -> new RegistryNotFoundException(idCliente));
-        // localizar os ids dos exercicios
-        List<Exercicio> exercicios = exercicioRepository.findAllById(idExercicios);
-        // regra de negocio: se id não exitir no banco não opera a transação
-
-        var idExerciciosNoBanco = exercicios.stream()
-                .map(Exercicio::getId)
-                .toList();
-
-        List<Long> idsNotExisting = new ArrayList<>();
-
-        for (Long id : idExercicios) {
-            if (!idExerciciosNoBanco.contains(id)) {
-                idsNotExisting.add(id);
-            }
-        }
-
-        if(!idsNotExisting.isEmpty()) {
-           throw new ExercicioIdsNotFoundException(idsNotExisting);
-        }
-
-        // crio cada associação (nova tabela de associacao) uma para cada id passado
-        cliente.setExercicios(exercicios);
-//        clientRepository.save()
-        // salva a associacao
-        clienteRepository.update(cliente, idCliente);
-        throw new ExercicioIdsNotFoundException(idsNotExisting);
-    }
-
-    public List<Exercicio> findAllExerciciosByClienteId(Long clienteId) {
-//        return clienteRepository.findById(clienteId)
-//                .stream()
-//                .map(Cliente::getExercicios)
-//                .flatMap(List::stream)
-//                .toList();
-        var clientes = exercicioRepository.findAllClienteMakingExerciseName("%" + "ros" + "%", Sort.by("nome").descending());
-
-        Pageable page = PageRequest.of(2, 10, Sort.by("nome").descending());
-        Page<Exercicio> exerciciosPage = exercicioRepository.findExercicioByNome("%" + "ros" + "%", page);
-        return exercicioRepository.findAllByClientId(clienteId);
-    }
+    List<Exercicio> findAllExerciciosByClienteId(Long clienteId);
 }
